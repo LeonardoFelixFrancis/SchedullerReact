a963583f3601ce4fc3a25ff318d27a2cca51f7e2
Leonardo <felixfranciscolt@gmail.com>
2025-06-02 08:30:04 -0300

    fix: was not showing the correct name on the profile options

--- src/components/ProfileOptions.tsx
+++ src/components/ProfileOptions.tsx
@@ -8,14 +8,17 @@ export default function ProfileOptions() {
     const { logout } = useAuth();
     const navigate = useNavigate();
 
+    const storedUser = localStorage.getItem("user");
+    const user = storedUser ? JSON.parse(storedUser) : null;
+
     const handleLogout = () => {
         logout();
         navigate('/');
     }
 
     return (
-        <div className="bg-white border border-gray-200 rounded shadow-md w-40 text-sm box-content flex flex-col gap-2">
-            <ProfileRow action={() => {}} ><b>Leonardo</b></ProfileRow>
+        <div className="bg-white border border-gray-200 rounded shadow-md min-w-[120px] w-fit text-sm box-content flex flex-col gap-2">
+            <ProfileRow action={() => {}} ><b>{user.name}</b></ProfileRow>
             <ProfileRow action={handleLogout} >
                 
                 <div className="flex">
